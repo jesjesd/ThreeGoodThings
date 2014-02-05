6d42fed45212f50848a10cd72b896e0cce636c16
jesjesd <jesdiesing@gmail.com>
2014-02-04 19:38:21 -0800

    adding templates and static files

--- tgt/tgt/settings.py
+++ tgt/tgt/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Setting Template Path, joining the BASE_DIR and the templates directory
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'TGTapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,6 +53,10 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'tgt.urls'
 
 WSGI_APPLICATION = 'tgt.wsgi.application'
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
 
 
 # Database
@@ -80,3 +87,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
